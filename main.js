abb05e4d3663f39c691ba9cdad750a3db4a0bc4d
rebeccapatek <rebeccapatek@gmail.com>
2019-11-19 11:29:46 -0600

    added some interactive js

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ *  Import the initializeDetailButtonEvents function
+ *  reference and then invoke it
+ */
+/**
+ *  Import the initializeDetailButtonEvents function
+ *  reference and then invoke it
+ */
+import initializeDetailButtonEvents from './scripts/dialog.js'
+import addMouseOverEventListeners from './scripts/highlight.js'
+
+
+initializeDetailButtonEvents()
+addMouseOverEventListeners()
